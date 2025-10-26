32b68f162d84916d77bb54687439fa3b5d13cac7
awais <awaiszahid684@gmail.com>
2025-10-26 20:30:38 +0500

    update terraform files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
@@ -11,4 +11,10 @@ variable "ami_id" {
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
   default     = "t2.large"
+}
+
+variable "vpc_var" {
+  description = "VPC ID where resources (like security groups) will be created"
+  type        = string
+  default     = "	vpc-0734d725f20e869b3" # Optional — you can leave blank and pass at runtime
 }
